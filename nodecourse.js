eff7e3ff48054beb9e29da3f46f3ffa970d7c36b
DESKTOP-G7JTIU0\victor <vmdlssnt@gmail.com>
2018-03-25 23:32:30 -0800

    currently at lesson 6 out of 14

--- nodecourse.js
+++ nodecourse.js
@@ -1,21 +1,55 @@
-//console.log('HELLO WORLD')
-//console.log(process.argv)
-let accumulation = 0;
-for (let i =0;i<process.argv.length;i++){
-	//console.log(process.argv[i])
-	if(i>=2){
-/*
-		if(process.argv[i].length>=2){
-			console.log(process.argv[i])
-			accumulation=Number(accumulation+process.argv[i].substring[0,2])
-		}else{
-*/
-		accumulation= Number(accumulation)+Number(process.argv[i])	
-		
-		}
-		
+
+/*LESSON 5*/
+var fs = require('fs');
+fs.readdir(process.argv[2],function(err,data){
+	if(!err){
+		data.map(function(file){
+			var stringsegments = file.split('.')
+			let extension = stringsegments[stringsegments.length-1]
+			if(file.includes('.')){
+				var argumentstringsegments = process.argv[3].split('.')	
+				let argumentextension = argumentstringsegments[argumentstringsegments.length-1]
+				if(argumentextension===extension){
+					console.log(file)
+				}
+			}
+
+		})
+
+	}
+})
+
+
+/*LESSON 4*/
+// var fs = require('fs');
+// fs.readFile(process.argv[2],function(error,data){
+// 	if(!error){
+// 		var arrayofsegments = (data.toString()).split('\n')
+// 		console.log(arrayofsegments.length-1)
+// 	}
 	
+// })
+
+/*LESSON 3*/
+// var fs = require('fs');
+// var buffer = fs.readFileSync(process.argv[2]);
+
+// var arrayofsegments = (buffer.toString()).split('\n');
+
+// console.log(arrayofsegments.length-1);
+
+
+/*LESSON 2*/
+// //console.log(process.argv)
+// let accumulation = 0;
+// for (let i =0;i<process.argv.length;i++){
+// 	//console.log(process.argv[i])
+// 	if(i>=2){
+
+
+// 		accumulation= Number(accumulation)+Number(process.argv[i])	
+//		}
+
 	
-}
-console.log(accumulation)
-//console.log(accumulation)
+// }
+// console.log(accumulation)
